d72c2e436aa6f9f970022af7c4405f04fcf58bee
Antonybyrt <antony.loussararian@gmail.com>
2025-08-13 12:52:15 +0200

    work please

--- server/config/database.config.ts
+++ server/config/database.config.ts
@@ -6,16 +6,6 @@ const sequelize = new Sequelize('Carlev', process.env.DATABASE_USERNAME as strin
     host: process.env.HOST,
     port: Number(process.env.PORT),
     dialect: 'mysql',
-    logging: console.log,
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-    retry: {
-        max: 3
-    }
     // dialectOptions: {
     //     socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
     // },
